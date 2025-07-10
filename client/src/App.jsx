1881b4476e3e47349464d0f8ed28a6995ada0781
Randima Dilshani <randimadilshani98@gmail.com>
2025-07-10 14:10:52 +0530

    Implemented Frontend login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/User/Login";
+import SignUp from "./components/User/SignUp";
+import LandingPage from "./pages/LandingPage";
+import Dashboard from "./pages/Dashboard";
+import CreateTasks from "./components/Tasks/CreateTasks";
+import TaskDetails from "./components/Tasks/TaskDetails";
+import CompletedTasks from "./components/Tasks/CompletedTasks";
+import EditTasks from "./components/Tasks/EditTasks";
+import InProgressTasks from "./components/Tasks/InProgressTasks";
+import ToDoTasks from "./components/Tasks/ToDoTasks";
+import TeamMembers from "./components/User/TeamMembers";
+import SpecificTask from "./components/Tasks/SpecificTask";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/createtasks" element={<CreateTasks />} />
+      <Route path="/taskdetails" element={<TaskDetails />} />
+      <Route path="/completedtasks" element={<CompletedTasks />} />
+      <Route path="/updateTask/:taskId" element={<EditTasks />} />
+      <Route path="/inprogresstasks" element={<InProgressTasks />} />
+      <Route path="/pendingtasks" element={<ToDoTasks />} />
+      <Route path="/team" element={<TeamMembers />} />
+      <Route path="/getTask/:taskId" element={<SpecificTask />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
